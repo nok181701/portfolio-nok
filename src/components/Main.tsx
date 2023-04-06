17b8ae7653ab236c3281a36449bb79308c1a65bb
祝井直樹 <nok181701@gmail.com>
2023-04-06 13:56:31 +0900

    ♻️tsxコンポーネント化

---
+++ src/components/Main.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import styles from "@/styles/Home.module.scss";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Main() {
+  return (
+    <div className={styles.center}>
+      <Image
+        className={styles.logo}
+        src="/next.svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+        priority
+      />
+      <div className={styles.thirteen}>
+        <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+      </div>
+    </div>
+  );
+}
