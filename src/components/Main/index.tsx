1917d1ce4b1ca133346bb334e352ba61660d49f9
祝井直樹 <nok181701@gmail.com>
2023-04-07 19:13:43 +0900

    ✨ ヘッダー実装

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,23 +1,8 @@
-import Image from "next/image";
 import styles from "src/components/Main/Main.module.scss";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export function Main() {
-  return (
-    <div className={styles.center}>
-      <Image
-        className={styles.logo}
-        src="/next.svg"
-        alt="Next.js Logo"
-        width={180}
-        height={37}
-        priority
-      />
-      <div className={styles.thirteen}>
-        <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-      </div>
-    </div>
-  );
+  return <div className={styles.center}></div>;
 }
