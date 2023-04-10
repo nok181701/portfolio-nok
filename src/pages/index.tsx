41827b43f186fd771ee47afdcbf618965995b3c7
祝井直樹 <nok181701@gmail.com>
2023-04-11 00:29:08 +0900

    ✨ フッター追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,16 @@ import styles from "src/styles/Home.module.scss";
 import { Inter } from "next/font/google";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
+import { Footer } from "src/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <>
-      <main className={styles.main}>
-        <Header />
-        <Main />
-      </main>
-    </>
+    <main className={styles.main}>
+      <Header />
+      <Main />
+      <Footer />
+    </main>
   );
 }
